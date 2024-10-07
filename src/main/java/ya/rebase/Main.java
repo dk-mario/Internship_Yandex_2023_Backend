360af04e8156c2513278ae530c6e2370d6194f6c
Dmitry Kartsev <dkmarioche@gmail.com>
2024-10-07 23:50:52 +0300

    Rebase
    add Hello from rebase method
    
    add class Main in package Rebasa

---
+++ src/main/java/ya/rebase/Main.java
@@ -0,0 +1,10 @@
+package ya.rebase;
+
+public class Main {
+<<<<<<< HEAD
+    public static void main(String[] args) {
+        System.out.println("Hello from rebase");
+    }
+=======
+>>>>>>> 7562488 (add class Main in package Rebasa)
+}
